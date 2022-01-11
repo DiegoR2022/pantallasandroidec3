4acb35c4f0a149074512b2a4c832b209eb64f8d6
DiegoR2022 <95902207+DiegoR2022@users.noreply.github.com>
2022-01-11 12:26:27 -0500

    EC3

---
+++ app/src/main/java/com/example/pantallasandroidec3/HomeFragment.java
@@ -0,0 +1,34 @@
+package com.example.pantallasandroidec3;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.button.MaterialButton;
+public class HomeFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.home_fragment, container, false);
+        MaterialButton settingsButton = view.findViewById(R.id.setting_button);
+
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((NavigationHost) getActivity()).navigateTo(new SettingsFragment(), false);
+            }
+        });
+
+        return view;
+    }
+}
+
+
+
